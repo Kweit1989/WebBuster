84b8ed4307206b664df0ad8276bf00782f7fd616
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2025-01-29 15:54:17 +0300

    swiper

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 new Swiper('.navigation', {
-    spaceBetween: 35,
+   
     slidesPerView: 'auto',
     centeredSlides: false, // Отключаем центрирование слайдов
     freeMode: true, // Позволяет прокручивать без жесткой привязки к слайдам
