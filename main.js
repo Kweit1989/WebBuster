9c7bf67b1bb4db2ecca563eefbde547f63517241
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2025-01-29 14:25:02 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+new Swiper('.navigation', {
+    spaceBetween: 35,
+    slidesPerView: 'auto',
+    centeredSlides: false, // Отключаем центрирование слайдов
+    freeMode: true, // Позволяет прокручивать без жесткой привязки к слайдам
+});
